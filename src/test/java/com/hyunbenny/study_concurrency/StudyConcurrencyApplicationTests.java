9797a4daa4f7c5845db16c6dd00d2f3b6d210834
hyunbenny <bluesky3268@naver.com>
2023-07-03 19:29:14 +0900

    initial commit

---
+++ src/test/java/com/hyunbenny/study_concurrency/StudyConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunbenny.study_concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyConcurrencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
